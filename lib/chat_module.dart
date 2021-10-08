49c97e9d57f1d27f0da7d6096ba22edbb2991fa5
hendi <hendi@xulu.co.id>
2021-10-08 14:02:26 +0700

    - refactor: codebase integration   - export sendbird user model class

--- lib/chat_module.dart
+++ lib/chat_module.dart
@@ -1,6 +1,10 @@
 library setoko_chat_package;
 
+// SendBird Internal
 export 'package:sendbird_sdk/constant/enums.dart';
+export 'package:sendbird_sdk/core/models/user.dart';
+
+// Chat Module
 export 'core/modules/chat/chat_module.dart';
 export 'core/interfaces/chat_interface.dart';
 export 'views/chat/chat_view.dart';
